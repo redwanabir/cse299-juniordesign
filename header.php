31952b5b7df6e92a9be7f1b6b1a23ffb8f1037e5
samia01 <48171304+samia01@users.noreply.github.com>
2019-03-19 23:13:51 +0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+<html>
+   <head>
+       
+       <title>HOME SERVICE</title>
+       <link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
+
+   	</head>
+   	<body>
+   	<nav class="navbar navbar-inverse">
+   		<div class="container">
+   		<div class="navbar-header">
+   			<a class="navbar-brand" herf="/">Codeigniter</a>
+   		</div>
+   		   <div id="navbar">
+   			<ul class="nav navbar-nav">
+   				<li><a href="/">Home</a></li>
+   				<li><a href="/">About</a></li>
+   				<li><a href="/">Profile</a></li>
+   			</ul>
+   		    </div>
+   	     </div>
+   	</nav>
